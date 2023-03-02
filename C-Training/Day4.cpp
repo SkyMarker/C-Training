89112b81ad7ebad39388016ccbdccf4ffe06accb
周祉浩 <102199464+SkyMarker@users.noreply.github.com>
2023-03-02 10:41:19 +0800

    Updata Day1&4

--- C-Training/Day4.cpp
+++ C-Training/Day4.cpp
@@ -1,3 +1,27 @@
 //
 // Created by Dell on 2023/3/1.
 //
+#include <iostream>
+using namespace std;
+
+// 全局变量声明
+int g = 99;
+
+// 函数声明
+int func();
+
+int main()
+{
+    // 局部变量声明
+    int g = 10;
+    //cout << g;
+    int kk = func();
+    cout << kk;
+    return 0;
+}
+
+// 函数定义
+int func()
+{
+    return g;
+}
